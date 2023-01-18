235cfd8d0457a2159bb0bc8139e39e885175acf8
Abdifahtaho <ibnucabdule@gmail.com>
2023-01-18 11:43:36 +0300

    bsdel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require(`express`)
 const homerouts = require(`./routes/index`)
 const app = express()
 app.set(`view engine`, `ejs`)
-const port = process.env.PORT || 2000
+const port = process.env.PORT || 1000
 app.listen(port)
 console.log(`its running now`);
 
